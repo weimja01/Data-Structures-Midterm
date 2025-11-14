2086522aa4569fba09185acdcfe227bd7a94cc9a
weimja01 <weimja01@yahoo.com>
2025-11-14 15:17:58 +0000

    game started working on methods

---
+++ game.cpp
@@ -0,0 +1,47 @@
+#include "game.h"
+
+Game::Game()
+{
+}
+
+void Game::setupTrail()
+{
+}
+
+void Game::addProblem(const Problem &prob)
+{
+}
+
+void Game::displayProblems()
+{
+}
+
+void Game::solveTopProblem()
+{
+}
+
+void Game::displayCurrentLocation()
+{
+}
+
+void Game::displayStatus()
+{
+}
+
+void Game::moveToNextLocation()
+{
+}
+
+bool Game::checkWinCondition()
+{
+    return false;
+}
+
+bool Game::checkLoseCondition()
+{
+    return false;
+}
+
+void Game::run()
+{
+}
--- game.h
+++ game.h
@@ -1,7 +1,41 @@
 #ifndef GAME_H
 #define GAME_H
+#include <iostream>
+#include <string>
+#include <problem.h>
+#include <location.h>
+#include <unorderdLinkedList.h>
+#include <linkedStack.h>
+#include <linkedListIterator.h>
+
+class Game
+{
+    private:
+    unorderedLinkedList<Location> trail;
+    linkedStack<Problem> problemStack;
+    linkedListIterator<Location> currentLocation;
+
+    int health;
+    int food;
+    bool gameOver;
+
+    public:
+    Game();
+    void setupTrail();
+    void addProblem(const Problem& prob);
+    void displayProblems();
+    void solveTopProblem();
+    void displayCurrentLocation();
+    void displayStatus();
+    void moveToNextLocation();
+    bool checkWinCondition();
+    bool checkLoseCondition();
+    void run();
+    
 
 
+
+};
 
 
 
