7ee992a72897a5656d0430c76c85bec53ac33f51
weimja01 <weimja01@yahoo.com>
2025-11-14 15:08:04 +0000

    problem test

--- test_locations.cpp
+++ test_locations.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "unorderdLinkedList.h"
 #include "location.h"
+#include "linkedStack.h"
+#include "problem.h"
 
 int main(){
     //unordered linked list
     unorderedLinkedList<Location> trail;
+    linkedStack<Problem> problemStack;
+
+    std::cout << "Is stack Empty? " << (problemStack.isEmptyStack() ?  "Yes" : "No") << std::endl;
+
+    Problem prob1("Broken Wagon", "Your wagon axle broke! must repair");
+    Problem prob2("illness", "Someone has dysentery");
+    problemStack.push(prob1);
+    problemStack.push(prob2);
+    std::cout << "Is stack Empty? " << (problemStack.isEmptyStack() ?  "Yes" : "No") << std::endl;
+
     Location loc1("Independence, Missouri", "Starting point. Time to head west!");
     Location loc2("Kansas River", "A River Crossing. Dangerous but necessary.");
 
@@ -14,7 +26,11 @@ int main(){
     trail.insert(loc2);
 
     trail.print(std::cout, "\n");
-    std::cout << loc2.description;
+    std::cout << loc2.description <<"\n";
+
+    Problem topProblem = problemStack.peek();
+    std::cout << topProblem << std::endl;
+
     return 0;
 
 }
